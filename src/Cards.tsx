7c709a69432916eb496a3bf6e37d7215229caee0
sashazmey <lordagoth@gmail.com>
2025-02-22 19:17:17 +0300

    Add files via upload

---
+++ src/Cards.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './style.scss';
+
+interface SomeDataDto {
+    title: string,  // Большие буквы
+    subtitle: string, //{subtitle}:{intVal}
+    intVal: number,
+}
+
+const myArray: SomeDataDto[] = [
+    {
+        title: "hello world",
+        subtitle: "warpal site",
+        intVal: 0
+    },
+    {
+        title: "hello pvp",
+        subtitle: "warpal site",
+        intVal: 1
+    },
+    {
+        title: "arena master",
+        subtitle: "gachi.jpg",
+        intVal: 2
+    },
+    {
+        title: "vodka",
+        subtitle: "voobrazhenie",
+        intVal: 3
+    },
+    {
+        title: "react",
+        subtitle: "javascript",
+        intVal: 4
+    },
+]
+
+export default function Сards() {
+    return (
+        <div className="container">
+            {myArray.map((card) => {
+                return (
+                    <div className="card">
+                        <div className="card__title">{card.title}</div>
+                        <div className="card__subtitle">{card.subtitle}: {card.intVal}</div>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
+
